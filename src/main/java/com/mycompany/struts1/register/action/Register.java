3b43703ac6316b723b750c4a3fa86b42d7627c31
Leo Ruelas Rojas <leoruelasr25@gmail.com>
2024-11-20 18:47:17 -0500

    save files

---
+++ src/main/java/com/mycompany/struts1/register/action/Register.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.struts1.register.action;
+
+import com.mycompany.struts1.model.Person;
+import com.opensymphony.xwork2.ActionSupport;
+
+/**
+ *
+ * @author leoru
+ */
+public class Register extends ActionSupport {
+    
+    private static final long serialVersionUID = 1L;
+    
+    private Person personBean;
+
+
+    public String execute() throws Exception {
+        return SUCCESS;
+    }
+
+    public Person getPersonBean() {
+        return personBean;
+    }
+    
+    public void setPersonBean(Person person) {
+        personBean = person;
+    }
+
+}
